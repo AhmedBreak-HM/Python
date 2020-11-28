d89282eff227d85a1c7e1667ae9c2db0cdd12aa5
ahmed break <ahmed_break8@yahoo.com>
2020-11-28 02:07:10 +0200

    python print

--- python.py
+++ python.py
@@ -1,2 +1,22 @@
+# helpe function
+# to get any information in python
+help(print)
+
+
 # first commit
-print('hello python')
+print('hello python','hello world')
+# change seprator
+print('python','sep',sep='>>>>>')
+# write in file
+# f=open('demofile.txt','w')
+# print('write in file', 'test',sep='***',file=f)
+
+
+# Variables
+# Variables is ref type to call new obj
+x=10
+print('x is int',x)
+y='string'
+print('y is',y)
+x=y
+print('this is ref type new obj from x',x)
